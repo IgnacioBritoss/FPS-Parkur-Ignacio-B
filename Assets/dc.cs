7a63e27e9bd708cd0eca6b05d1889ae464b261e8
IgnacioBritoss <167232030+IgnacioBritoss@users.noreply.github.com>
2025-05-23 09:06:15 -0300

    nada

---
+++ Assets/dc.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dc : MonoBehaviour
+{
+    void OnColisionEnter(Collision collision)
+    {
+        Debug.Log("bien");
+        Destroy(gameObject);
+    }
+
+  
+}
