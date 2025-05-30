217321911b7fedbd08ff2c4a0296a272f932a87d
IgnacioBritoss <167232030+IgnacioBritoss@users.noreply.github.com>
2025-05-30 09:04:30 -0300

    todo listo

---
+++ Assets/mercan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mercan : MonoBehaviour
+{
+    public int precio = 10;
+    public dinero dinero;
+
+
+     void Start()
+    {
+        dinero = FindObjectOfType<dinero>();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            dinero.UpdateMoney(precio);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/dinero.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dinero : MonoBehaviour
+{
+    public float playermoney;
+    public void UpdateMoney  (float amount)
+    {
+       
+
+        if (playermoney + amount < 0)
+        {
+
+        }
+        else
+        {
+            playermoney += amount;
+        }
+
+    }
+      
+}
